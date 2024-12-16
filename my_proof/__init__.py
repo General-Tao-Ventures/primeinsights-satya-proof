13fcc799fc5800f4ff081a3e438e957136919a18
ronx-labs <ron.sam624@gmail.com>
2024-12-16 13:51:57 +0000

    fix: disabled attribute on proof response

--- my_proof/__init__.py
+++ my_proof/__init__.py
@@ -75,8 +75,8 @@ class Proof:
         remote_log(self.config, json.dumps(self.proof_response.model_dump(), indent=2))
         
         # Additional (public) properties to include in the proof about the data
-        self.proof_response.attributes = {
-            'category_scores': self.proof_response.quality, # Redundant with byte array
-        }
+        # self.proof_response.attributes = {
+        #     'category_scores': self.proof_response.quality, # Redundant with byte array
+        # }
 
         return self.proof_response
